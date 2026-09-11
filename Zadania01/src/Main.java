c3add44f0df474dfb5466dc8d75b6a0d5c43e69e
Adam Marzec <test@test.test>
2026-09-11 14:07:35 +0200

    Zadania 1

---
+++ Zadania01/src/Main.java
@@ -0,0 +1,15 @@
+void main() {
+
+    String imie1 = "Ania";
+    String imie2 = "Bartek";
+    String imie3 = "Kasia";
+    for (int i = 1; i<=3; i++) {
+        if (i == 1) {
+            System.out.println(imie1);
+        } else if (i == 2) {
+            System.out.println(imie2);
+        } else {
+            System.out.println(imie3);
+        }
+    }
+}
